789ece0822b623ea44d46700c9911170e715fd83
kubo-hide-kun <hide44041@gmail.com>
2019-12-24 11:28:25 +0900

    Give up login implementation

--- app/controllers/Security.java
+++ app/controllers/Security.java
@@ -1,11 +1,8 @@
 package controllers;
 
-import models.User;
-
 public class Security extends Secure.Security {
 
 	static boolean authenticate(String address, String password) {
-		User user = User.find("byEnail",address).first();
-		return user != null && user.password.equals(password);
+		return true;
     }
 }
